3f457132ae09e26c61c6fa1896d5bbe161a8e70f
mudusararshad37 <hafizmudusar37@gmail.com>
2022-09-18 21:54:07 -0700

    1st commit

---
+++ app.js
@@ -0,0 +1,149 @@
+// 1=========>write a ts program to print all natural numbers from 1 to n using while loop
+// var n1:number=1
+// let n=50
+// while(n1<=n){
+//     console.log(n1);
+//     n1++
+// }
+// 2========>write a ts program to print all natural numbers from n to 1 in reverse using while loop
+// var n1:number=1
+// let n=50
+// while(n>=n1){
+//     console.log(n);
+//     n--
+// }
+// 3==============>write a ts program to print all alphbets from a to z using while loop
+// var a:number=65
+// while(a>=65&&a<=90){
+//     console.log(String.fromCharCode(a));
+//     a++
+//     
+// }
+// 4=============>. Write a ts program to print all even numbers between 1 to 100. - using while loop
+// var num1:number=1
+// while(num1<=100){
+//     if(num1%2==0){
+//         console.log(num1);
+//     }
+//     num1++
+// }
+// 5=============>. Write a ts program to print all odd numbers between 1 to 100. - using while loop
+// var num1:number=1
+// while(num1<=100){
+//     if(num1%2!=0){
+//         console.log(num1);
+//     }
+//     num1++
+// }
+// 6=========>. Write a ts program to find sum of all natural numbers between 1 to n
+// var num:number=0
+// for (let index = 1; index <=5 ; index++) {
+//     console.log(index);
+//     num= num+index 
+// }
+// console.log(num);
+// 7=============>. Write a ts program to find sum of all even numbers between 1 to n
+// var num1:number=0
+// var num2:number=10
+// for (let i = num1; i <=num2 ; i++) {
+//   if(i%2==0){
+//     console.log(i);
+// num1=num1+i
+// }
+// }
+// console.log(num1);
+// 8=============>. Write a ts program to find sum of all odd numbers between 1 to n
+// var num1:number=0
+// var num2:number=10
+// for (let i = num1; i <=num2 ; i++) {
+//   if(i%2!=0){
+//     console.log(i);
+// num1=num1+i
+// }
+// }
+// console.log(num1);
+// 9==========>. Write a ts program to print multiplication table of any number.
+// var num:number=Number(prompt("type here a number"))
+// for( let i=1;i<=10;i++){
+//     console.log(num+"*"+i+"="+num*(i));
+// }
+// 10=========>. Write a ts program to count number of digits in a number
+// var a:number=12345
+// console.log(a.toString().length);
+// 11==========>. Write a ts program to find first and last digit of a number
+// var num:number=1589
+//  var lastnumber:number=num%10
+//  while(num>=10){
+//     num=(num / 10);
+//  }
+// console.log("first number",Math.floor(num));
+// console.log("last number",lastnumber);
+// 12=================>. Write a ts program to find sum of first and last digit of a number.
+// var num:number=1580
+//  var lastnumber:number=num%10
+//  while(num>=10){
+//     num=parseInt(num / 10);
+//  }
+// var sum:number=num+lastnumber
+// console.log(sum);
+// 13==================>. Write a ts program to swap first and last digits of a number.
+var Notcomplete = 2;
+// var num:number=1587
+//  var lastNumber:number=num%10
+//  var firstNumber:number=num
+//  while(firstNumber>=10){
+//     var firstNumber=parseInt(firstNumber/ 10);
+//     var lastNumber:number=firstNumber
+//    }
+//    // var num:number=firstNumber
+//    // var firstNumber:number=lastNumber
+//    console.log(num);
+//    // var num:number=swap
+//    // //  firstNumber=lastNumber
+//    // var lastNumber=firstNumber
+//    // console.log(num);
+// var num=swap
+// console.log("first number",first);
+// console.log("last number",last);
+// 14=================>. Write a ts program to calculate sum of digit of a number.
+//??????????????????????? how to find sum of all module of anumber????????????????????
+// var num:number=34809
+// var sum:number=0
+// while(num>=1){
+//    sum=sum+Math.floor(num%10)
+//    num=(num/10)
+// }
+//    console.log(sum);
+// 15==========================>. Write a ts program to calculate product of digits of a number
+// var num:number=123
+// var multiplication:number=1
+// while(num>=1){
+//    multiplication*=Math.floor(num%10)
+//    num=num/10
+// }
+// console.log(multiplication);
+// 16==========================>. Write a ts program to enter a number and print its reverse.
+//  var num:string="1234"
+// var reverse=num.split("").reverse().join("")
+// // while(num!=0){
+// //    reverse=(reverse*10)+(num%10);
+// //    num=num/10;
+// // }
+// console.log((reverse));
+// 17===================>. Write a ts program to check whether a number is palindrome or not.
+// var num:number=112
+// var num1:string= num.toString()
+// var num2=num1.split("").reverse().join("")
+// if(num1==num2){
+//    console.log("number is palindrome");
+// }
+// else{
+//    console.log("number is not a palindrome");
+// }
+// 18==============>. Write a ts program to find frequency of each digit in a given integer.
+var num = 1122;
+var num1 = num.toString();
+var num2 = num1.split("");
+console.log(num2);
+for (var i = 0; i <= num2.length; i++) {
+}
